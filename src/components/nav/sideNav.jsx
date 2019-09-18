be2b97bdcfa211f7cd42a64b9617d5aacad9a036
Edmond O'Flynn <oflynned@tcd.ie>
2019-09-18 14:03:20 +0100

    implement icon selection in side nav

--- src/components/nav/sideNav.jsx
+++ src/components/nav/sideNav.jsx
@@ -1,51 +1,63 @@
 import React from "react";
 
+import AppsIcon from "../../assets/images/appsIcon";
+
 import "./sideNav.css";
+import SettingsIcon from "../../assets/images/settingsIcon";
+import ScheduleIcon from "../../assets/images/scheduleIcon";
+import TeamIcon from "../../assets/images/teamIcon";
 
 const icons = [
     {
         title: "Projects",
         url: "/projects",
-        icon: "round-apps-24px.svg"
+        unselectedIcon: <AppsIcon fill={"#A0AAB0"}/>,
+        selectedIcon: <AppsIcon fill={"#02BCC8"}/>
     },
     {
         title: "Team",
         url: "/team",
-        icon: "baseline-people_outline-24px.svg"
+        unselectedIcon: <TeamIcon fill={"#A0AAB0"}/>,
+        selectedIcon: <TeamIcon fill={"#02BCC8"}/>
     },
     {
         title: "Roadmap",
         url: "/roadmap",
-        icon: "baseline-schedule-24px.svg"
+        unselectedIcon: <ScheduleIcon fill={"#A0AAB0"}/>,
+        selectedIcon: <ScheduleIcon fill={"#02BCC8"}/>
     },
     {
         title: "Settings",
         url: "/settings",
-        icon: "baseline-settings-20px.svg"
+        unselectedIcon: <SettingsIcon fill={"#A0AAB0"}/>,
+        selectedIcon: <SettingsIcon fill={"#02BCC8"}/>
     }
 ];
 
-const loadSvg = (path, fillColour) => {
-    const svg = require("../../assets/images/" + path);
-    return svg;
-}
-
-const SideNav = ({selectedIndex}) =>
+const SideNav = ({selectedIndex, onClick}) =>
     <div className={"side-nav"}>
-        <img className={"action icon hamburger"} src={require("../../assets/images/baseline-hamburger-24px.svg")}/>
+        <img alt={"menu link"}
+             className={"action icon hamburger"}
+             src={require("../../assets/images/baseline-hamburger-24px.svg")}/>
         <ul>
             {
-                icons.map(({title, url, icon}, index) =>
-                    <li>
-                        <div className={"action circle " + (selectedIndex === index ? "selected" : "unselected")}>
-                            <img className={"icon"} src={loadSvg(icon)}/>
-                        </div>
-                    </li>
+                icons.map(({title, url, selectedIcon, unselectedIcon}, index) => {
+                        const isSelected = selectedIndex === index;
+                        return (
+                            <li key={index} onClick={() => onClick(url)}>
+                                <div className={"action circle " + (isSelected ? "selected" : "unselected")}>
+                                    <div className={"icon"}>
+                                        {isSelected ? selectedIcon : unselectedIcon}
+                                    </div>
+                                </div>
+                            </li>
+                        );
+                    }
                 )
             }
         </ul>
 
-        <img className={"action icon log-out"}
+        <img alt={"logout link"} className={"action icon log-out"}
              src={require("../../assets/images/baseline-power_settings_new-24px.svg")}/>
     </div>;
 
