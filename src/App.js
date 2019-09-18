8111ac7c6d529301d01be8e0206657013370cf47
Edmond O'Flynn <oflynned@tcd.ie>
2019-09-18 14:02:49 +0100

    update routes

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import {BrowserRouter, Route, Switch} from "react-router-dom";
 import DevelopmentBoard from "./layouts/developmentBoard";
 
 import './App.css';
+import TeamManager from "./layouts/teamManager";
 
 function App() {
     return (
         <BrowserRouter>
             <div className="App">
                 <Switch>
-                    <Route exact path={"/"} component={DevelopmentBoard}/>
+                    <Route exact path={"/projects"} component={DevelopmentBoard}/>
+                    <Route exact path={"/team"} component={TeamManager}/>
                 </Switch>
             </div>
         </BrowserRouter>
