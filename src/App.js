df86cefa5a0f38428e2ee5c2787e576e3add9897
Edmond O'Flynn <oflynned@tcd.ie>
2019-10-02 18:33:01 +0200

    move pages from layouts/ to pages/

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 
-import DevelopmentBoard from "./layouts/developmentBoard";
+import DevelopmentBoard from "./pages/dashboard/developmentBoard";
 
 import './App.css';
-import TeamManager from "./layouts/teamManager";
+import TeamManager from "./pages/dashboard/teamManager";
+import Login from "./pages/userIdentity/login";
 
 function App() {
     return (
         <BrowserRouter>
             <div className="App">
                 <Switch>
+                    <Route exact path={["/", "/login"]} component={Login}/>
                     <Route exact path={"/projects"} component={DevelopmentBoard}/>
                     <Route exact path={"/team"} component={TeamManager}/>
                 </Switch>
--- src/pages/dashboard/developmentBoard.jsx
+++ src/pages/dashboard/developmentBoard.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import {Redirect} from "react-router-dom";
 
-import SideNav from "../components/nav/sideNav";
+import SideNav from "../../components/nav/sideNav";
 
 import "./developmentBoard.css";
 
